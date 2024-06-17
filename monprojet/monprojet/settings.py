1c6f3a8a0b7c5e1637ce3a1dfcb5d50cf95b7366
SImonJ09 <jude-simon.agohoundje@epitech.eu>
2024-06-17 23:09:30 +0100

    test de deploiements deploier

--- monprojet/monprojet/settings.py
+++ monprojet/monprojet/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'monapp',
     'monprojet',
-    'storages',
+#     'storages',
 ]
 
 MIDDLEWARE = [
@@ -146,13 +146,13 @@ EMAIL_HOST_PASSWORD = '&mMLvIKy8qzc'
 
 
 # MinIO settings
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = 'minioadmin'
-AWS_SECRET_ACCESS_KEY = 'minioadmin'
-AWS_STORAGE_BUCKET_NAME = 'your-bucket-name'
-AWS_S3_ENDPOINT_URL = 'http://localhost:9000'
-AWS_S3_REGION_NAME = 'us-east-1'
-AWS_S3_SIGNATURE_VERSION = 's3v4'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = 'public-read'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS_ACCESS_KEY_ID = 'minioadmin'
+# AWS_SECRET_ACCESS_KEY = 'minioadmin'
+# AWS_STORAGE_BUCKET_NAME = 'your-bucket-name'
+# AWS_S3_ENDPOINT_URL = 'http://localhost:9000'
+# AWS_S3_REGION_NAME = 'us-east-1'
+# AWS_S3_SIGNATURE_VERSION = 's3v4'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL = 'public-read'
